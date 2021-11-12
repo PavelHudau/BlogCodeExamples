9ea579d14b06cd03e620e7777840063579cac5ed
Pavel Hudau <paylikn@gmail.com>
2021-11-12 06:59:44 -0500

    fist singleton examples

---
+++ Patterns/Singleton/GreedySingleton.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace Singleton
+{
+    public class GreedySingleton
+    {
+        // New instance is created when CLR loads the class. Thread safety is guaranteed.
+        private static GreedySingleton _instance = new GreedySingleton();
+
+        private GreedySingleton() { }
+
+        // The property just returns created instance.
+        public static GreedySingleton Instance => _instance;
+    }
+
+    public class GreedySingletonWithStartupInit
+    {
+        private static object lockObj = new object();
+        private static GreedySingletonWithStartupInit _instance;
+
+        private GreedySingletonWithStartupInit() { }
+
+        /// <summary>
+        /// The property just returns created and initialized
+        /// GreedySingletonWithStartupInit instance.
+        /// If the instance is not initialized InvalidOperationException is thrown.
+        /// </summary>
+        public static GreedySingletonWithStartupInit Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new InvalidOperationException(
+                        $"{nameof(GreedySingletonWithStartupInit)} is not initialized ");
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Initializes GreedySingletonWithStartupInit one time.
+        /// Must be invoked on application startup.
+        /// </summary>
+        public static void InitializeOnAppStartup()
+        {
+            lock (lockObj)
+            {
+                if (_instance == null)
+                {
+                    _instance = new GreedySingletonWithStartupInit();
+                    _instance.Initialize();
+                }
+                else
+                {
+                    throw new InvalidOperationException(
+                        $"{nameof(GreedySingletonWithStartupInit)} already initialized");
+                }
+            }
+        }
+
+        private void Initialize()
+        {
+            // Heavy initialization logic goes here.
+        }
+    }
+}
---
+++ Patterns/Singleton/UsageExamples.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Singleton
+{
+    public class UsageExamples
+    {
+        public void UsingReadonlySingleton()
+        {
+            var proxyUri = $"{MyAppConfiguration.Instance.ProxyHost}:{MyAppConfiguration.Instance.ProxyPort}";
+            Console.Write($"Proxy address: {proxyUri}");
+        }
+
+        public void UsingPubSub()
+        {
+            // Subscribing to Loading event
+            PubSubAsSingleton.Instance.LoadingEvent += (sender, args) =>
+            {
+                if (args.LoadedProgress >= 1)
+                {
+                    Console.WriteLine($"Loaded!");
+                }
+                else
+                {
+                    Console.WriteLine($"Loading... {args.LoadedProgress * 100}%");
+                }
+            };
+
+            // Publishing Loading events
+            for (var i = 0; i <= 100; i++)
+            {
+                PubSubAsSingleton.Instance.RaiseLoadingEvent(this, i / 100.0);
+            }
+        }
+    }
+}
