ebc6b702a38dd4cd37ed737b23ca7a6956945429
Iryna Hudau <Airan.inn1@gmail.com>
2023-04-19 09:36:29 -0400

    Update index.ts

--- Debouncer/index.ts
+++ Debouncer/index.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express = require('express');
+import path = require('path'); 
 
 const server = express();
 const port = 5005;
 
 server.get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile(path.resolve(_dirname,'index.html');
 });
 
 server.listen(port, () => {
