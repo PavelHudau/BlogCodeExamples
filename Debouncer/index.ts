a6579470b3fdf946f44af1c30cef49f02047840f
Pavel Hudau <paylikn@gmail.com>
2023-04-20 20:59:48 -0400

    fixed script loading

--- Debouncer/index.ts
+++ Debouncer/index.ts
@@ -1,11 +1,15 @@
-import express = require('express');
+import express from 'express';
+import path from 'path';
 
 const server = express();
 const port = 5005;
 
+server.use(express.static(path.join(__dirname, 'public')));
+
 server.get('/', function (req, res) {
     res.sendFile('index.html', { root: __dirname });
 });
+
 
 server.listen(port, () => {
     console.info(`Listening on port ${port}`);
