8951cf952f4b089ec99c82b6b51315e32759048b
Pavel Hudau <paylikn@gmail.com>
2023-04-20 20:38:14 -0400

    working

--- Debouncer/index.ts
+++ Debouncer/index.ts
@@ -1,11 +1,10 @@
 import express = require('express');
-import path = require('path'); 
 
 const server = express();
 const port = 5005;
 
 server.get('/', function (req, res) {
-    res.sendFile(path.resolve(_dirname,'index.html');
+    res.sendFile('index.html', { root: __dirname });
 });
 
 server.listen(port, () => {
