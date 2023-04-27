9bcf545696f0a7c8f4c66c5e6f8e8df770f9acbc
Pavel Hudau <paylikn@gmail.com>
2023-04-27 06:23:18 -0400

    more code

---
+++ Debouncer/public/debouncer.ts
@@ -0,0 +1,66 @@
+export class CancellationToken {
+    #isCancelled: boolean;
+
+    public get isCancelled() { return this.#isCancelled };
+
+    public cancel(): void {
+        this.#isCancelled = true;
+    }
+}
+
+export class DeBouncer {
+    readonly #maxDelayMs: number;
+    readonly #minDelayMs: number;
+    readonly #delayNoise: number;
+    #latestToken: CancellationToken = new CancellationToken();
+    #latestDebounceTimeMs: number;
+
+    constructor(
+        maxDelayMs: number = 3000,
+        minDelayMs: number = 0,
+        delayNoise: number = 40) {
+        // Make sure #maxDelayMs is greater than 0;
+        this.#maxDelayMs = Math.max(maxDelayMs, 0);
+        // Make sure #minDelayMs is greater than 0 and less or equal to #maxDelayMs;
+        this.#minDelayMs = Math.min(Math.max(minDelayMs, 0), this.#maxDelayMs);
+        // #delayNoise must be less or equal to #maxDelayMs,
+        // otherwise all delays will be considered as noise and will be ignored.
+        this.#delayNoise = Math.min(delayNoise, this.#maxDelayMs);
+    }
+
+    public async debounce(): Promise<CancellationToken> {
+        this.#latestToken.cancel();
+        const currentToken = new CancellationToken();
+        this.#latestToken = currentToken;
+        const delayByMs = this.#nextDelayMs();
+        if (delayByMs > 0 && delayByMs >= this.#delayNoise) {
+            await delay(delayByMs);
+        }
+
+        return Promise.resolve(currentToken);
+    }
+
+    get #nowTimeInMs(): number {
+        return new Date().getTime();
+    }
+
+    #nextDelayMs(): number {
+        const nowTimeInMs = this.#nowTimeInMs;
+        // Frequency is a difference in milliseconds between subsequent actions.
+        const frequency = nowTimeInMs - this.#latestDebounceTimeMs;
+        this.#latestDebounceTimeMs = nowTimeInMs;
+        const P0 = 2;
+        const k = -2.5;
+        let delayMs = Math.floor(P0 * Math.pow(Math.E, k * frequency));
+        delayMs = Math.min(delayMs, this.#maxDelayMs);
+        return Math.max(delayMs, this.#minDelayMs);
+    }
+}
+
+async function delay(delayByMs: number) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, delayByMs);
+    });
+}
--- Debouncer/public/ui-scripts.ts
+++ Debouncer/public/ui-scripts.ts
@@ -1,58 +1,36 @@
-export class CancellationToken {
-    #isCancelled: boolean;
+import { DeBouncer, CancellationToken } from "./debouncer";
 
-    public get isCancelled() { return this.#isCancelled };
-
-    public cancel(): void {
-        this.#isCancelled = true;
-    }
+export async function onProductTileClickNaive(eventArgs) {
+    showLoading();
+    const product = await fetchProduct(eventArgs.productId);
+    hideLoading();
+    showSlideOutProductPanel(product);
 }
 
-export class Debouncer {
-    readonly #maxDelayMs: number;
-    readonly #minDelayMs: number;
-    readonly #delayNoise: number;
-    #latestToken: CancellationToken = new CancellationToken();
-    #latestDebounceTimeMs: number;
+let productFetchCancellationToken = new CancellationToken();
 
-    constructor(maxDelayMs: number = 1000, minDelayMs: number = 0, delayNoise: number = 35) {
-        // Make sure #maxDelayMs is greater than 0;
-        this.#maxDelayMs = Math.max(maxDelayMs, 0);
-        // Make sure #minDelayMs is greater than 0 and less or equal to #maxDelayMs;
-        this.#minDelayMs = Math.min(Math.max(minDelayMs, 0), this.#maxDelayMs);
-        // #delayNoise must be less or equal to #maxDelayMs,
-        // otherwise all delays will be considered as noise and will be ignored.
-        this.#delayNoise = Math.min(delayNoise, this.#maxDelayMs);
+export async function onProductTileClickWithCancellation(eventArgs) {
+    // Cancel previously created token.
+    productFetchCancellationToken.cancel();
+
+    // Create and keep current token.
+    const currentToken = new CancellationToken();
+    productFetchCancellationToken = currentToken;
+
+    const product = await fetchProduct(eventArgs.productId);
+
+    // If current token was cancelled by next Product Tile click, then
+    // just exit.
+    if (currentToken.isCancelled) {
+        return;
     }
 
-    public async debounce() {
-        this.#latestToken.cancel();
-        const currentToken = new CancellationToken();
-        this.#latestToken = currentToken;
-        const delayByMs = this.#nextDelayMs();
-        if(delayByMs > 0 && delayByMs >= this.#delayNoise) {
-            await delay(delayByMs);
-        }
+    // Fetch was not cancelled, so we can show Product in
+    // the slide out panel.
+    showSlideOutProductPanel(product);
+}
 
-        Promise.resolve(currentToken);
-    }
-
-    get #nowTimeInMs(): number {
-        return new Date().getTime();
-    }
-
-    #nextDelayMs(): number {
-        const nowTimeInMs = this.#nowTimeInMs;
-        const delta = nowTimeInMs - this.#latestDebounceTimeMs;
-        this.#latestDebounceTimeMs = nowTimeInMs;
-        const scale = 270000000;
-        let delayMs = Math.floor(delta > 0 ? (scale / Math.pow(delta, 1.9)) : this.#maxDelayMs);
-        delayMs = Math.min(delayMs, this.#maxDelayMs);
-        return Math.max(delayMs, this.#minDelayMs);
-    }
- }
-
-async function delay(delayByMs: number) {
+async function delayByMs(delayByMs: number) {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(true);
@@ -60,3 +38,117 @@ async function delay(delayByMs: number) {
     });
 }
 
+export async function onProductTileClickWithCancellationAndDelay(eventArgs) {
+    productFetchCancellationToken.cancel();
+
+
+    const currentToken = new CancellationToken();
+    productFetchCancellationToken = currentToken;
+
+    // Delay real service call by 1 second
+    await delayByMs(1000);
+
+    // If new click happened before delay expired, then just exit.
+    if (currentToken.isCancelled) {
+        return;
+    }
+
+    const product = await fetchProduct(eventArgs.productId);
+
+    if (currentToken.isCancelled) {
+        return;
+    }
+
+    showSlideOutProductPanel(product);
+}
+
+const maxDelayMs = 3000; // 3 seconds
+const minDelayMs = 0;
+// We need to remember time of last click to be able
+// to calculate frequency.
+let latestDebounceTimeMs = new Date().getTime();
+
+function nextDelayMs(): number {
+    const nowTimeInMs = new Date().getTime();
+    // Frequency is a difference in milliseconds between
+    // subsequent clicks.
+    const frequency = nowTimeInMs - latestDebounceTimeMs;
+    latestDebounceTimeMs = nowTimeInMs;
+
+    const P0 = 2;
+    const k = -2.5;
+    let delayMs = Math.floor(P0 * Math.pow(Math.E, k * frequency));
+
+    // Ensure delayMs is within the boundaries between
+    // minDelayMs and maxDelayMs.
+    delayMs = Math.min(delayMs, maxDelayMs);
+    return Math.max(delayMs, minDelayMs);
+}
+
+export async function onProductTileClickWithCancellationAndExponentialDelay(eventArgs) {
+    productFetchCancellationToken.cancel();
+
+    const currentToken = new CancellationToken();
+    productFetchCancellationToken = currentToken;
+
+    // It does not make sense to delay for very short period of time.
+    // We settled on 40 ms empirically.
+    const ignoreDelayLessThanMs = 40;
+    const delayMs = nextDelayMs();
+    if (delayMs > ignoreDelayLessThanMs) {
+        await delayByMs(delayMs);
+    }
+
+    if (currentToken.isCancelled) {
+        return;
+    }
+
+    const product = await fetchProduct(eventArgs.productId);
+
+    if (currentToken.isCancelled) {
+        return;
+    }
+
+    showSlideOutProductPanel(product);
+}
+
+// Create a DeBouncer instance for each user action 
+// ou would like to  De-Bounce.
+const productTileClickDeBouncer = new DeBouncer();
+
+export async function onProductTileClickWithDebBouncer(eventArgs) {
+    const currentToken = await productTileClickDeBouncer.debounce();
+
+    if (currentToken.isCancelled) {
+        return;
+    }
+
+    const product = await fetchProduct(eventArgs.productId);
+
+    if (currentToken.isCancelled) {
+        return;
+    }
+
+    showSlideOutProductPanel(product);
+}
+
+interface Product {
+}
+
+function showLoading() {
+    // Disables all click event handlers.
+}
+
+function hideLoading() {
+    // Disables all click event handlers.
+}
+
+function showSlideOutProductPanel(product) {
+    // Shows product data.
+}
+
+async function fetchProduct(productId: string): Promise<Product> {
+    // Fetches product from the server.
+    return {
+    };
+}
