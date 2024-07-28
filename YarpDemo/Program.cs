42fcabed8280669878cd9cf122b1400f65fb36cf
Pavel Hudau <paylikn@gmail.com>
2024-07-28 17:36:26 -0400

    mutate requests

--- YarpDemo/Program.cs
+++ YarpDemo/Program.cs
@@ -1,6 +1,23 @@
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddTransforms(builderCtx =>
+    {
+        // Add new header
+        builderCtx.AddRequestHeader("new-header", "new-header-value");
+        builderCtx.AddRequestTransform(transformCtx =>
+        {
+            if (transformCtx.ProxyRequest?.Content != null)
+            {
+                // Update header-to-update header
+                transformCtx.ProxyRequest.Content.Headers.Remove("header-to-update");
+                transformCtx.ProxyRequest.Content.Headers.Add("header-to-update", "value");
+            }
+            return ValueTask.CompletedTask;
+        });
+    });
 
 var app = builder.Build();
 
